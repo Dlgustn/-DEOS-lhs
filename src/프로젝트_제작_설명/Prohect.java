663db79e827d97dab2cc8105057981fc2eae2f65
Dlgustn <skfnxhwpfh@naver.com>
2023-06-19 15:46:54 +0900

    프로젝트 제작 설명

---
+++ src/프로젝트_제작_설명/Prohect.java
@@ -0,0 +1,15 @@
+package 프로젝트_제작_설명;
+
+public class Prohect {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		- 내용:
+//
+//		- 현재날짜: 2023-06-19
+//
+//		- 제작자: LHS
+	}
+
+}
